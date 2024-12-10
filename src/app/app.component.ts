6cc2bc0b2b7ddc71fbc4aa8e5364eabe7942f54a
“Zahed75” <zahedhasan.cs@gmail.com>
2024-12-11 03:34:30 +0600

    ng class added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-
-import {DataBindingComponent} from './components/data-binding/data-binding.component';
 import {NgIfComponent} from './components/ng-if/ng-if.component';
 import {NgForComponent} from './components/ng-for/ng-for.component';
+import {NgClassComponent} from './components/ng-class/ng-class.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NgIfComponent,NgForComponent],
+  imports: [NgIfComponent,NgForComponent,NgClassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
