24dbd3bd789c9402773a85d61e2ba415ae01e293
“Zahed75” <zahedhasan.cs@gmail.com>
2024-12-11 03:19:28 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import {DataBindingComponent} from './components/data-binding/data-binding.component';
+import {NgIfComponent} from './components/ng-if/ng-if.component';
+import {NgForComponent} from './components/ng-for/ng-for.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [NgIfComponent,NgForComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'test-project';
+}
